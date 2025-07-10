7531db963d20179176e67f261104955b8417d06c
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-10 14:20:48 +0300

    Reorganized App structure and added tree of components for counter button

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -32,29 +32,25 @@ function App() {
   )
 }
 
-
 export function Nasa() {
     return (
         <>
-            <title>Welcome to Nasa!</title>
-            <img
-                src="https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg"
-                alt="NASA Logo"
-                width="200"
+            <TitleImageTemplate
+                title="Welcome to NASA!"
+                image="https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg"
+                p1="You are at NASA"
+                p2="Join us!"
             />
-            <p>Welcome to Nasa!</p>
-            <p>You are here</p>
+
+            <CounterTemplate/>
+
+            <Component1/>
         </>
     )
 }
 
 // @ts-ignore
 export function TitleImageTemplate({ title, image, p1, p2 }) {
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        setCount(Number(localStorage.getItem("count")))
-    }, []);
 
     return (
         <>
@@ -66,24 +62,67 @@ export function TitleImageTemplate({ title, image, p1, p2 }) {
             />
             <p>{p1}</p>
             <p>{p2}</p>
-
-            <button onClick={() => {
-                localStorage.setItem("count", String(count + 1));
-                setCount((count) => count + 1);
-                }
-            }>
-                count is {count}
-            </button>
-
-            <button onClick={() => {
-                localStorage.setItem("count", "0");
-                setCount(0);
-            }
-            }>
-                reset count
-            </button>
         </>
     )
+}
+
+function CounterTemplate() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setCount(Number(localStorage.getItem("count")))
+    }, []);
+
+    return (
+        <>
+            <button onClick={() => {
+                localStorage.setItem("count", String(count + 1))
+                setCount((count) => count + 1)
+            }}>
+                count is {count}
+            </button>
+            <button onClick={() => {
+                localStorage.setItem("count", "0")
+                setCount(0)
+            }}>
+                reset count
+            </button>
+        </>)
+}
+
+function Component1()  {
+
+    // State is and should be here (root of complex tree)
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount((count) => count + 1);
+
+    return (
+        <div>
+            <Component2 increment={increment} />
+            <Component3 count={count} />
+        </div>
+    );
+}
+
+// @ts-ignore
+function Component2({ increment }) {
+    return <button onClick={increment}>increment me!</button>;
+}
+
+// @ts-ignore
+function Component3({ count }) {
+    return (
+        <div>
+            <Component4 count={count} />
+            <p>counter is being updated</p>
+        </div>
+    );
+}
+
+// @ts-ignore
+function Component4 ({ count }) {
+    return <div>count is {count}</div>;
 }
 
 
--- marsappspa/src/main.tsx
+++ marsappspa/src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import  { TitleImageTemplate } from './App.tsx'
+import  { Nasa } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TitleImageTemplate
-        title="Welcome to NASA!"
-        image="https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg"
-        p1="You are at NASA"
-        p2="Join us!"
-    />
+    <Nasa />
   </StrictMode>,
 )
